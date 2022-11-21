4fbade35fa6c5fa299f3c5ed540279f1b82cfeb2
barry <barybtw@gmail.com>
2022-11-21 20:46:50 +0100

    added tw

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from "next/app";
 import CustomLayout from "../lib/ssr-layout";
 
 import "../style.css";
-import "../App.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
